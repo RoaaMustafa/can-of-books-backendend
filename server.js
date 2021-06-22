068cbea7767ed5d779576c8b68b86dec2bfd016a
RoaaMustafa <labushanab14@gmail.com>
2021-06-22 19:55:13 +0300

    finish lab13

--- server.js
+++ server.js
@@ -2,12 +2,12 @@
 
 require("dotenv").config();
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+app.use(express.json());
+// const getBooksHandler = require('./modules/user');
 
-const getBooksHandler = require('./modules/user');
-
-const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3010;
@@ -15,14 +15,33 @@ mongoose.connect("mongodb://localhost:27017/books", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+const {
+  handleDeleteBook,
+  getBooksHandler,
+  handleAddBook,
+} = require('./modules/user');
+app.get("/books", getBooksHandler);
+
+//http://localhost:3010/book?email=abed
+// server.get('/book', bookHandler)
+app.post('/addbook', handleAddBook);
+app.delete('/deletebook/:index', handleDeleteBook);
+
+// const {
+//   getBooksHandler,
+//   handleAddBook,
+//   handleDeleteBooks,
+// } = require('./modules/user');
 
 //localhost:3010/
 app.get("/", homeHandler);
 function homeHandler(req, res) {
   res.send("Home Route");
 }
-
 // http://localhost:3010/books?email=labushanab14@gmail.com
-app.get("/books", getBooksHandler);
+
+// app.post('/addbook', handleAddBook);
+// app.delete('/deletebook/:index', handleDeleteBooks);
+
 
 app.listen(PORT || 3010, () => console.log(`listening on ${PORT}`));
